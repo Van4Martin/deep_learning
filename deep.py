a91384b799e739fe4ce9cc92b1d1f7c1e0d6bf29
Martin <van4martin@gmail.com>
2023-05-08 00:12:20 +0000

    First day over

--- deep.py
+++ deep.py
@@ -30,9 +30,49 @@ and context for the model, which can lead to \
 more detailed and relevant outputs.
 """
 prompt = f"""
-Summarize the text delimited by triple backticks \ 
+Summarize the text delimited by html tags \ 
 into a single sentence.
-```{text}```
+<{text}>
 """
 response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Generate a list of six made-up book titles along \ 
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
+"""
+response = get_completion(prompt)
+print(response)
+
+
+text_2 = f"""
+The sun is shining brightly today, and the birds are \
+singing. It's a beautiful day to go for a \ 
+walk in the park. The flowers are blooming, and the \ 
+trees are swaying gently in the breeze. People \ 
+are out and about, enjoying the lovely weather. \ 
+Some are having picnics, while others are playing \ 
+games or simply relaxing on the grass. It's a \ 
+perfect day to spend time outdoors and appreciate the \ 
+beauty of nature.
+"""
+prompt = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text_2}\"\"\"
+"""
+response = get_completion(prompt)
+print("Completion for Text 2:")
 print(response)
