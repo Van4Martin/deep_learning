b195e34786820621197868ef948580effd6dfd17
Martin <van4martin@gmail.com>
2023-05-13 03:14:10 +0000

    good

--- deep.py
+++ deep.py
@@ -17,62 +17,15 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     return response.choices[0].message["content"]
 
 
-text = f"""
-You should express what you want a model to do by \ 
-providing instructions that are as clear and \ 
-specific as you can possibly make them. \ 
-This will guide the model towards the desired output, \ 
-and reduce the chances of receiving irrelevant \ 
-or incorrect responses. Don't confuse writing a \ 
-clear prompt with writing a short prompt. \ 
-In many cases, longer prompts provide more clarity \ 
-and context for the model, which can lead to \ 
-more detailed and relevant outputs.
-"""
 prompt = f"""
-Summarize the text delimited by html tags \ 
-into a single sentence.
-<{text}>
+Write a function that reads a text file and prints it to the POSIX standard output.
+
+Prototype: ssize_t read_textfile(const char *filename, size_t letters);
+where letters is the number of letters it should read and print
+returns the actual number of letters it could read and print
+if the file can not be opened or read, return 0
+if filename is NULL return 0
+if write fails or does not write the expected amount of bytes, return 0
 """
 response = get_completion(prompt)
-print(response)
-
-prompt = f"""
-Generate a list of six made-up book titles along \ 
-with their authors and genres. 
-Provide them in JSON format with the following keys: 
-book_id, title, author, genre.
-"""
-response = get_completion(prompt)
-print(response)
-
-
-text_2 = f"""
-The sun is shining brightly today, and the birds are \
-singing. It's a beautiful day to go for a \ 
-walk in the park. The flowers are blooming, and the \ 
-trees are swaying gently in the breeze. People \ 
-are out and about, enjoying the lovely weather. \ 
-Some are having picnics, while others are playing \ 
-games or simply relaxing on the grass. It's a \ 
-perfect day to spend time outdoors and appreciate the \ 
-beauty of nature.
-"""
-prompt = f"""
-You will be provided with text delimited by triple quotes. 
-If it contains a sequence of instructions, \ 
-re-write those instructions in the following format:
-
-Step 1 - ...
-Step 2 - …
-…
-Step N - …
-
-If the text does not contain a sequence of instructions, \ 
-then simply write \"No steps provided.\"
-
-\"\"\"{text_2}\"\"\"
-"""
-response = get_completion(prompt)
-print("Completion for Text 2:")
 print(response)
